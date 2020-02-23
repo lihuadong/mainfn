af86bd2486cafb6024b2e33447413b178aa959bd
lihuadong <lihuadongv@163.com>
2020-02-23 22:31:47 +0800

    ava 提供了一种对象序列化的机制，该机制中，一个对象可以被表示为一个字节序列，该字节序列包括该对象的数据、有关对象的类型的信息和存储在对象中数据的类型。
    将序列化对象写入文件之后，可以从文件中读取出来，并且对它进行反序列化，也就是说，对象的类型信息、对象的数据，还有对象中的数据类型可以用来在内存中新建对象。
    
    整个过程都是 Java 虚拟机（JVM）独立的，也就是说，在一个平台上序列化的对象可以在另一个完全不同的平台上反序列化该对象。
    
    类 ObjectInputStream 和 ObjectOutputStream 是高层次的数据流，它们包含反序列化和序列化对象的方法。

---
+++ mainfn/src/mainfn/java/io/SerializeDemo.java
@@ -0,0 +1,49 @@
+package mainfn.java.io;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+class Employee implements java.io.Serializable
+{
+   /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+public String name;
+   public String address;
+   public transient int SSN;
+   public int number;
+   
+   public void mailCheck()
+   {
+      System.out.println("Mailing a check to " + name
+                           + " " + address);
+   }
+}
+
+public class SerializeDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    Employee e = new Employee();
+	      e.name = "Reyan Ali";
+	      e.address = "Phokka Kuan, Ambehta Peer";
+	      e.SSN = 11122333;
+	      e.number = 101;
+	      try
+	      {
+	         FileOutputStream fileOut =
+	         new FileOutputStream("/Users/lihuadong/Documents/Temp/employee.ser");
+	         ObjectOutputStream out = new ObjectOutputStream(fileOut);
+	         out.writeObject(e);
+	         out.close();
+	         fileOut.close();
+	         System.out.printf("Serialized data is saved in /lihuadong/Documents/Temp/employee.ser");
+	      }catch(IOException i)
+	      {
+	          i.printStackTrace();
+	      }
+	}
+
+}
