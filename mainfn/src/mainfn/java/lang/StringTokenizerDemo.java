730c31c72f6b562f73a3d269c709f5d58ee4d850
lihuadong <lihuadongv@163.com>
2020-03-02 23:59:29 +0800

    code

--- mainfn/src/mainfn/java/lang/StringTokenizerDemo.java
+++ mainfn/src/mainfn/java/lang/StringTokenizerDemo.java
@@ -2,10 +2,13 @@ package mainfn.java.lang;
 
 import java.util.StringTokenizer;
 
+//Java 中我们可以使用 StringTokennizer 设置不同分隔符来分隔字符串，默认的分隔符是：空格、制表符（\t）、换行符(\n）、回车符（\r）。
+//以下实例演示了 StringTokennizer 使用空格和等号来分隔字符串：
+
 public class StringTokenizerDemo {
 	   public static void main(String[] args) {
 		   
-	        String str = "This is String , split by StringTokenizer, created by fullstackeer";
+	        String str = "This is String , split by StringTokenizer, created\tby fullstackeer";
 	        StringTokenizer st = new StringTokenizer(str);
 	 
 	        System.out.println("----- 通过空格分隔 ------");
