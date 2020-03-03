87c5c52bfa37f492c04e888d67cb441a9eef928c
lihuadong <lihuadongv@163.com>
2020-03-03 13:19:11 +0800

    Java 所有的组成部分都需要名字。类名、变量名以及方法名都被称为标识符。
    关于 Java 标识符，有以下几点需要注意：
    
    所有的标识符都应该以字母（A-Z 或者 a-z）,美元符（$）、或者下划线（_）开始
    首字符之后可以是字母（A-Z 或者 a-z）,美元符（$）、下划线（_）或数字的任何字符组合
    关键字不能用作标识符
    标识符是大小写敏感的
    合法标识符举例：age、$salary、_value、__1_value
    非法标识符举例：123abc、-salary

---
+++ mainfn/src/mainfn/java/Base.java
@@ -0,0 +1,16 @@
+package mainfn.java;
+
+public class Base {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String age;
+        String $salar;
+        String _value;
+        String __1_value;
+        String $$abc;
+        
+        
+	}
+
+}
