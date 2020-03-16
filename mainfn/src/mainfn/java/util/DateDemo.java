a561bb6def8fa0fcd1d52ab2329defe1cc0cffe0
lihuadong <lihuadongv@163.com>
2020-03-16 23:58:23 +0800

    DateDemo 更新

--- mainfn/src/mainfn/java/util/DateDemo.java
+++ mainfn/src/mainfn/java/util/DateDemo.java
@@ -1,5 +1,6 @@
 package mainfn.java.util;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DateDemo {
@@ -18,5 +19,10 @@ public class DateDemo {
 	       System.out.println(date.toString());
 	       // 使用 toString() 函数显示日期时间
 	       System.out.println(date.toString());
+	       
+	       Date dNow = new Date( );
+	       SimpleDateFormat ft = new SimpleDateFormat ("yyyy-MM-dd hh:mm:ss");
+	  
+	       System.out.println("当前时间为: " + ft.format(dNow));
 	   }
 }
