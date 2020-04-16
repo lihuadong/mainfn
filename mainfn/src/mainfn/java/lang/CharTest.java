5225c2c6e5d5dcdd20b3f4f1939264d64e021f0f
lihuadong <lihuadongv@163.com>
2020-04-17 00:42:31 +0800

    一个汉字，使用三个字节

---
+++ mainfn/src/mainfn/java/lang/CharTest.java
@@ -0,0 +1,18 @@
+package mainfn.java.lang;
+
+public class CharTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String str  = "拙";
+        byte [] strBytes = str.getBytes();
+        System.out.println(strBytes.length);
+        
+        
+        String str1  = "[Facepalm]";
+        byte [] strBytes1 = str1.getBytes();
+        System.out.println(strBytes1.length);
+        
+        }
+
+}
